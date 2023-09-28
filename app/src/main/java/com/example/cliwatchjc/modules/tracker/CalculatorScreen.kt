f6517ae0393e9f01e8c2e0f2b3c80de58af2a7f7
NatalieTanJieYin <natalietjy-wm22@student.tarc.edu.my>
2023-09-28 21:33:45 +0800

    Edited calculator screen

--- app/src/main/java/com/example/cliwatchjc/modules/tracker/CalculatorScreen.kt
+++ app/src/main/java/com/example/cliwatchjc/modules/tracker/CalculatorScreen.kt
@@ -2,20 +2,27 @@ package com.example.cliwatchjc.modules.tracker
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -43,176 +50,232 @@ fun CalculatorScreen(calculatorViewModel: CalculatorViewModel) {
 
     LazyColumn(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(start = 16.dp, end = 16.dp)
+            .padding(start = 28.dp, end = 28.dp)
+            .fillMaxWidth()
     ) {
         item {
-            Spacer(modifier = Modifier.height(80.dp))
+            Spacer(modifier = Modifier.height(40.dp))
         }
 
         item {
-            Column(
-                modifier = Modifier
-                    .padding(top = 60.dp)
-                    .fillMaxWidth()
+            // Title
+            Text(
+                text = "Carbon Footprint Calculator",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                ),
+
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            // Transportation Input
+            Text(
+                text = "Transportation",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+            ExposedDropdownMenuBox(
+                expanded = transportationMenuExpanded,
+                onExpandedChange = { transportationMenuExpanded = !transportationMenuExpanded },
             ) {
-                // Title
-                Text(
-                    text = "Carbon Footprint Calculator",
-                    style = TextStyle(
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                    ),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-
-                // Transportation Input
-                Text(
-                    text = "Transportation",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
-                )
-                ExposedDropdownMenuBox(
-                    expanded = transportationMenuExpanded,
-                    onExpandedChange = { transportationMenuExpanded = !transportationMenuExpanded },
-                ) {
-                    TextField(
-                        modifier = Modifier.menuAnchor(),
-                        readOnly = true,
-                        value = selectedTransportation,
-                        onValueChange = {},
-                        label = { Text("Label") },
-                        trailingIcon = {
-                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = transportationMenuExpanded)
-                        },
-                        colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                    )
-                    ExposedDropdownMenu(
-                        expanded = transportationMenuExpanded,
-                        onDismissRequest = { transportationMenuExpanded = false }
-                    ) {
-                        transportationOptions.forEach { selectionOption ->
-                            DropdownMenuItem(text = { Text(selectionOption) }, onClick = {
-                                selectedTransportation = selectionOption
-                                transportationMenuExpanded = false
-                            })
-                        }
-                    }
-                }
                 TextField(
-                    value = transportationDistance,
-                    onValueChange = { transportationDistance = it },
-                    label = { Text("Distance Travelled (km/week)") }
-                )
-
-                // Food Input
-                Text(
-                    text = "Food",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
-                )
-                ExposedDropdownMenuBox(
-                    expanded = dietMenuExpanded,
-                    onExpandedChange = { dietMenuExpanded = !dietMenuExpanded },
-                ) {
-                    TextField(
-                        modifier = Modifier.menuAnchor(),
-                        readOnly = true,
-                        value = selectedDiet,
-                        onValueChange = {},
-                        label = { Text("Label") },
-                        trailingIcon = {
-                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = dietMenuExpanded)
-                        },
-                        colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                    )
-                    ExposedDropdownMenu(
-                        expanded = dietMenuExpanded,
-                        onDismissRequest = { dietMenuExpanded = false }
-                    ) {
-                        dietOptions.forEach { selectionOption ->
-                            DropdownMenuItem(text = { Text(selectionOption) }, onClick = {
-                                selectedDiet = selectionOption
-                                dietMenuExpanded = false
-                            })
-                        }
-                    }
-                }
-                TextField(
-                    value = foodSpending,
-                    onValueChange = { foodSpending = it },
-                    label = { Text("Food Spending (\$/week)") }
-                )
-
-                // Energy Input
-                Text(
-                    text = "Energy",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
-                )
-                TextField(
-                    value = electricityUsage,
-                    onValueChange = { electricityUsage = it },
-                    label = { Text("Electricity Usage (kWh)") }
-                )
-
-                TextField(
-                    value = naturalGasUsage,
-                    onValueChange = { naturalGasUsage = it },
-                    label = { Text("Natural Gas Usage (MJ)") }
-                )
-
-                TextField(
-                    value = firewoodUsage,
-                    onValueChange = { firewoodUsage = it },
-                    label = { Text("Firewood Usage (kg)") }
-                )
-
-                // Calculate Button
-                Button(
-                    onClick = {
-                        val transportationValue = calculatorViewModel.calculateTransportation(
-                            selectedTransportation,
-                            transportationDistance
-                        )
-                        val foodValue =
-                            calculatorViewModel.calculateFood(selectedDiet, foodSpending)
-                        val energyValue = calculatorViewModel.calculateEnergy(
-                            electricityUsage,
-                            naturalGasUsage,
-                            firewoodUsage
-                        )
-                        val totalCarbonFootprint = transportationValue + foodValue + energyValue
-                        calculatorViewModel.setCarbonFootprint(totalCarbonFootprint)
-                        showResult = true
+                    modifier = Modifier.menuAnchor(),
+                    readOnly = true,
+                    value = selectedTransportation,
+                    onValueChange = {},
+                    label = { Text("Mode") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = transportationMenuExpanded)
                     },
-                    modifier = Modifier.padding(top = 16.dp)
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = transportationMenuExpanded,
+                    onDismissRequest = { transportationMenuExpanded = false }
                 ) {
-                    Text("Calculate")
+                    transportationOptions.forEach { selectionOption ->
+                        DropdownMenuItem(text = { Text(selectionOption) }, onClick = {
+                            selectedTransportation = selectionOption
+                            transportationMenuExpanded = false
+                        })
+                    }
                 }
+            }
+            TextField(
+                value = transportationDistance,
+                onValueChange = { transportationDistance = it },
+                label = { Text("Distance Travelled (km/week)") }
+            )
 
-                // Display Carbon Footprint if showResult is true
-                if (showResult) {
-                    val carbonFootprint = calculatorViewModel.carbonFootprint.value
-                    Text("Carbon Footprint: $carbonFootprint CO2e tons")
-
-                    // Display tips here
-                    Text("Here are some tips for you to further reduce your carbon footprint:")
-                    Text("Consume local and seasonal products")
-                    Text("Try the train for your next holiday")
-                    Text("Limit and recycle your waste")
+            // Food Input
+            Text(
+                text = "Food",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+            ExposedDropdownMenuBox(
+                expanded = dietMenuExpanded,
+                onExpandedChange = { dietMenuExpanded = !dietMenuExpanded },
+            ) {
+                TextField(
+                    modifier = Modifier.menuAnchor(),
+                    readOnly = true,
+                    value = selectedDiet,
+                    onValueChange = {},
+                    label = { Text("Diet Type") },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = dietMenuExpanded)
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                )
+                ExposedDropdownMenu(
+                    expanded = dietMenuExpanded,
+                    onDismissRequest = { dietMenuExpanded = false }
+                ) {
+                    dietOptions.forEach { selectionOption ->
+                        DropdownMenuItem(text = { Text(selectionOption) }, onClick = {
+                            selectedDiet = selectionOption
+                            dietMenuExpanded = false
+                        })
+                    }
                 }
+            }
+            TextField(
+                value = foodSpending,
+                onValueChange = { foodSpending = it },
+                label = { Text("Food Spending (\$/week)") }
+            )
+
+            // Energy Input
+            Text(
+                text = "Energy",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            )
+            TextField(
+                value = electricityUsage,
+                onValueChange = { electricityUsage = it },
+                label = { Text("Electricity Usage (kWh)") }
+            )
+
+            TextField(
+                value = naturalGasUsage,
+                onValueChange = { naturalGasUsage = it },
+                label = { Text("Natural Gas Usage (MJ)") }
+            )
+
+            TextField(
+                value = firewoodUsage,
+                onValueChange = { firewoodUsage = it },
+                label = { Text("Firewood Usage (kg)") }
+            )
+
+            // Calculate Button
+            Button(
+                onClick = {
+                    val transportationValue = calculatorViewModel.calculateTransportation(
+                        selectedTransportation,
+                        transportationDistance
+                    )
+                    val foodValue =
+                        calculatorViewModel.calculateFood(selectedDiet, foodSpending)
+                    val energyValue = calculatorViewModel.calculateEnergy(
+                        electricityUsage,
+                        naturalGasUsage,
+                        firewoodUsage
+                    )
+                    val totalCarbonFootprint = transportationValue + foodValue + energyValue
+                    calculatorViewModel.setCarbonFootprint(totalCarbonFootprint)
+                    showResult = true
+                },
+                modifier = Modifier.padding(top = 16.dp)
+            ) {
+                Text("Calculate")
+            }
+
+            // Display Result and Tips
+            if (showResult) {
+                ResultAndTipsSection(calculatorViewModel)
             }
         }
     }
 }
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+fun ResultAndTipsSection(calculatorViewModel: CalculatorViewModel) {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        // Display Carbon Footprint with larger text
+        val carbonFootprint = calculatorViewModel.carbonFootprint.value
+        Text(
+            text = "Carbon Footprint:",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        Text(
+            text = "$carbonFootprint CO2e tons",
+            style = TextStyle(
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        // Add some spacing between the result and tips
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Tips Section
+        Text(
+            text = "Tips to Reduce Your Carbon Footprint:",
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        // LazyColumn for displaying tips
+        LazyColumn {
+            items(tipsList) { tip ->
+                Tip(tip)
+            }
+        }
+        }
+    }
+
+
+@Composable
+fun Tip(text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Info,
+            contentDescription = null,
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = text)
+    }
+}
+
+val tipsList = listOf(
+    "Consume local and seasonal products",
+    "Try the train for your next holiday",
+    "Limit and recycle your waste"
+)
+
 
 @Preview
 @Composable
