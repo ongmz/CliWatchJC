a96400a3e2400704f00d6d0f1118519499ad17ad
NatalieTanJieYin <natalietjy-wm22@student.tarc.edu.my>
2023-09-30 15:23:48 +0800

    Edited summary screen

--- app/src/main/java/com/example/cliwatchjc/modules/tracker/SummaryScreen.kt
+++ app/src/main/java/com/example/cliwatchjc/modules/tracker/SummaryScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,13 +14,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -67,31 +69,58 @@ fun SummaryScreen() {
         BarChart(
             data = barChartData,
             modifier = Modifier
-                .padding(16.dp)
+                .padding(20.dp)
                 .fillMaxWidth()
                 .height(200.dp)
-                .align(Alignment.CenterHorizontally)
+                .align(Alignment.CenterHorizontally),
+            axisLabelFontSize = 12.sp
         )
 
-        // Display percentage values below the bar chart
-        Text(
-            text = "Transportation: ${"%.2f".format(transportationPercentage)}%",
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Card(
             modifier = Modifier
-                .padding(8.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = "Energy: ${"%.2f".format(energyPercentage)}%",
-            modifier = Modifier
-                .padding(8.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = "Food: ${"%.2f".format(foodPercentage)}%",
-            modifier = Modifier
-                .padding(8.dp)
-                .align(Alignment.CenterHorizontally)
-        )
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                // Summary Title
+                Text(
+                    text = "Summary",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    ),
+
+                    modifier = Modifier.padding(bottom = 20.dp)
+                )
+                // Display percentage values below the bar chart
+                Text(
+                    text = "Transportation: ${"%.2f".format(transportationPercentage)}%",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.Start),
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = "Energy: ${"%.2f".format(energyPercentage)}%",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.Start),
+                    fontSize = 16.sp
+                )
+                Text(
+                    text = "Food: ${"%.2f".format(foodPercentage)}%",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.Start),
+                    fontSize = 16.sp
+                )
+            }
+        }
     }
 }
 
@@ -100,8 +129,9 @@ fun BarChart(
     data: List<Pair<String, Float>>,
     modifier: Modifier = Modifier,
     barColor: Color = Color.Blue,
-    barWidth: Float = 50f,
-    barSpacing: Float = 16f
+    barWidth: Float = 80f,
+    barSpacing: Float = 100f,
+    axisLabelFontSize: TextUnit = 16.sp
 ) {
     val maxValue = data.maxByOrNull { it.second }?.second ?: 0f
     val maxYValue = ((maxValue / 20).toInt() + 1) * 20
@@ -118,14 +148,15 @@ fun BarChart(
         val paint = Paint()
 
         // Draw y-axis labels and lines
-        val yLabelSpace = 40.dp.toPx() // Adjust this value to your needs
+        val yLabelSpace = 3.dp.toPx() // Adjust this value to your needs
         for (i in 0..4) {
             val labelText = (i * 20).toString()
             val textWidth = paint.measureText(labelText)
-            val labelX = 10.dp.toPx()
-            val labelY = size.height - (i * (size.height - yLabelSpace) / 4) - 6.dp.toPx()
+            val labelX = 0.dp.toPx()
+            val labelY = size.height - (i * (size.height - yLabelSpace) / 4) - 4.dp.toPx()
 
-            // Draw y-axis labels
+            // Draw y-axis labels with adjusted font size
+            paint.textSize = axisLabelFontSize.toPx()
             drawIntoCanvas { canvas ->
                 canvas.nativeCanvas.drawText(labelText, labelX, labelY, paint)
             }
@@ -149,21 +180,12 @@ fun BarChart(
                 index * (barSpacing + barUnitWidth) + barSpacing + barWidth / 2 - textWidth / 2
             val labelY = size.height + 20.dp.toPx()
 
-            // Draw x-axis labels
+            // Draw x-axis labels with adjusted font size
+            paint.textSize = axisLabelFontSize.toPx()
             drawIntoCanvas { canvas ->
                 canvas.nativeCanvas.drawText(labelText, labelX, labelY, paint)
             }
         }
-
-        val xAxisLabelY = size.height + 40.dp.toPx()
-
-        // Draw x-axis line
-        drawLine(
-            color = Color.Gray,
-            start = Offset(10.dp.toPx(), xAxisLabelY),
-            end = Offset(size.width - 10.dp.toPx(), xAxisLabelY),
-            strokeWidth = 1.dp.toPx()
-        )
 
         data.forEachIndexed { index, (_, value) ->
             val barHeight = (value / maxYValue) * (size.height - yLabelSpace)
@@ -177,7 +199,7 @@ fun BarChart(
                 color = barColor,
                 topLeft = Offset(startX, endY),
                 size = Size(barWidth, barHeight),
-                style = Stroke(2.dp.toPx())
+                style = Fill
             )
         }
     }
